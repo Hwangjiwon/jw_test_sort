ae0280164151f90b1ef4f4e20d419986e8d56e66
jwhwang <googims33@naver.com>
2017-01-01 21:29:21 +0900

    failover

--- test_sort.c
+++ test_sort.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-int* insert_sort(int *arr){
+int* insert_sort(int *arr, int size){
     int i;
-    for(i=1; i<(sizeof(arr)/sizeof(int)); i++){
+    for(i=1; i<size; i++){
         int tmp = arr[i];
         int j;
         for(j=i-1; j>=0; j--){
@@ -16,11 +16,11 @@ int* insert_sort(int *arr){
      return arr;
 }
 
-int* bubble_sort(int *arr){
+int* bubble_sort(int *arr, int size){
     int i;
-    for(i=0; i<(sizeof(arr)/sizeof(int)); i++){
+    for(i=0; i<size; i++){
         int j, tmp;
-        for(j=0; j=sizeof(arr)/sizeof(int)-i-1; j++){
+        for(j=0; j=size-i-1; j++){
             if(arr[j] > arr[j+1]){
                 tmp = arr[j];
                 arr[j] = arr[j+1];
@@ -31,12 +31,12 @@ int* bubble_sort(int *arr){
     return arr;
 }
 
-int* select_sort(int *arr){
+int* select_sort(int *arr, int size){
     int i,min,tmp=0;
-    for(i=0; i<(sizeof(arr)/sizeof(int)-1); i++){
+    for(i=0; i<size-1; i++){
         int j;
         min = arr[i];
-        for(j=i; j<(sizeof(arr)/sizeof(int)-1); j++){
+        for(j=i; j<size-1; j++){
             if(min > arr[j+1]){
                 min = arr[j+1];
                 tmp = j+1;
@@ -50,48 +50,51 @@ int* select_sort(int *arr){
 
 int main(int argc, char *argv[]) {
     int *arr;
-    int size = atoi(argv[4]);
+    char *sort = argv[1];
+    char *opt = argv[2];
+    int size = atoi(argv[3]);
     int i;
     
-    arr = (int*)malloc(sizeof(int)*size);
-    printf("size = %d   ",size);
-    printf("argv[3]= %s",argv[3]);
-    if(strncmp(argv[3],"same",4)){
-        printf("qwer");
-        for(i=0;i<(sizeof(arr)/sizeof(int));i++)
-        {    arr[i] = size;
-        
-        }
-        for(i=0;i<sizeof(arr)/sizeof(int);i++)
-            printf("dkdkdk :%d ",arr[i]);
+    if(argc != 4){
+        printf("segmentation error");
+        return -1;
     }
-    else if(strncmp(argv[3],"asc",3)){
-        for(i=0;i<(sizeof(arr)/sizeof(int));i++)
+    
+    arr = (int*)malloc(sizeof(int)*size);
+    if(strncmp(opt,"same",4)==0){
+        for(i=0;i<size;i++)
+          arr[i] = size;
+    }
+    else if(strncmp(opt,"asc",3)==0){
+        for(i=0;i<size;i++)
             arr[i] = i;
     }
-    else if(strncmp(argv[3],"desc",4)){
-        for(i=0;i<(sizeof(arr)/sizeof(int));i++)
-            arr[i] = size-i;
+    else if(strncmp(opt,"desc",4)==0){
+        for(i=0;i<size;i++)
+            arr[i] = size-i-1;
     }
-    else if(strncmp(argv[3],"rand",4)){
-        for(i=0;i<(sizeof(arr)/sizeof(int));i++)
+    else if(strncmp(opt,"rand",4)==0){
+        for(i=0;i<size;i++)
             arr[i] = rand()%size;
     }
     
-    else printf("error");
-    printf("hello\n");
+    else{
+        printf("init_option error");
+        return -1;
+    }
+     
+    if(strncmp(sort,"ins",3)==0)
+        arr = insert_sort(arr,size);
+    else if(strncmp(sort,"bub",3)==0)
+        arr = bubble_sort(arr,size);
+    else if(strncmp(sort,"sel",3)==0)
+        arr = select_sort(arr,size);
+    else{
+         printf("sort_option error");
+         return -1;
+    }
     
-    if(strncmp(argv[2],"ins",3))
-       arr = insert_sort(arr);
-    else if(strncmp(argv[2],"bub",3))
-       arr = bubble_sort(arr);
-    else if(strncmp(argv[2],"sel",3))
-       arr = select_sort(arr);
-    else printf("error");
-    
-    printf("bye\n");
-    
-    for(i=0;i<sizeof(arr)/sizeof(int);i++)
+    for(i=0;i<size;i++)
         printf("%d ",arr[i]);
 
     return 0;
