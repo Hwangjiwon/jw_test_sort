95613a879c069ea02fb3c4405bdb4f3a4fba8bee
jwhwang <googims33@naver.com>
2017-01-06 19:07:57 +0900

    Add the option Ver2

--- test_sort.c
+++ test_sort.c
@@ -2,13 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include "test_sort.h"
 
+char *value[3] = {NULL};
+int input_check(int argc, char** argv);
 void init_option(int *arr, int size, char *opt);
 void insert_sort(int *arr, int size);
 void bubble_sort(int *arr, int size);
 void select_sort(int *arr, int size);
 void sort_option(int *arr, int size, char *sort);
-
+void print_t(long clock);
 
 int main(int argc, char *argv[]) {
     int *arr;
@@ -18,18 +21,20 @@ int main(int argc, char *argv[]) {
     int i;
 
     clock_t t;
-
+    
     t=clock();
-
-    if(argc != 4){
+/*
+    if(argc < 4){
         printf("segmentation error\n");
         return -1;
     }
-    
-    sort = argv[1];
-    opt = argv[2];
-    size = atoi(argv[3]);
-
+*/
+     if( -1 ==  input_check(argc,argv)){
+         return -1;
+     }
+    sort = value[0];
+    opt = value[1];
+    size = atoi(value[2]);
 
     arr = (int*)malloc(sizeof(int)*size);
 
@@ -38,11 +43,11 @@ int main(int argc, char *argv[]) {
     sort_option(arr,size,sort);
 
     t = clock()-t;
+    t = t/T;
     printf("%s : %ld ms\n",sort,t);
 
     return 0;
 }
-
 
 void init_option(int *arr, int size, char* opt){
     int i;
@@ -125,3 +130,63 @@ void sort_option(int *arr, int size, char* sort){
          return;
     }
 }
+
+int input_check(int argc, char** argv)
+{
+	int i,j,k;
+
+	if(argc == 1)
+	{	
+		printf("사용법 출력");
+		return -1;
+	}
+	else if (argc % 2 == 0 || 0 == strncmp(argv[argc-1],"-",1)) //argc가 홀수 이거나 argv[마지막]이 옵션일때 error
+	{
+		printf("ERROR: INPUT THE VALUE\n");
+		return -1;
+	}
+	else
+	{
+		for (i = 1; i < argc; i++) {
+			k = i%2; //k==1 홀수   k==0 짝수
+			j = (i+1)%2;
+			if (k == 1 && 0 == strncmp(argv[i], "-", 1)) //argv[홀]이 옵션인가
+			{ 				
+				if (j == 0 && 0 != strncmp(argv[i+1], "-", 1)) //argv[짝]이 값인가
+				{
+					
+//					printf("[%s][%s]\n",argv[i],argv[i+1]);
+					
+					if(0==strcmp(argv[i],"-a"))
+						value[0] = argv[i+1];
+					if(0==strcmp(argv[i],"-o"))
+						value[1] = argv[i+1];
+					if(0==strcmp(argv[i],"-s"))
+						value[2] = argv[i+1];
+
+				}
+				else printf("input error\n");
+			}
+		}
+		if (value[0] == NULL) {
+			printf("print rules\n");
+			return -1;
+		}
+		else {
+			if (value[1] == NULL) {
+				value[1] = "random";
+			}
+			if (value[2] == NULL) {
+				value[2] = "32768";
+			}
+		}
+/*		
+		printf("\n#########\n");
+		for (i = 0; i < 3; i++) {
+			printf("value[%d]=%s\n",i,value[i]);
+		}
+*/		
+	}
+	return 0;
+}
+
