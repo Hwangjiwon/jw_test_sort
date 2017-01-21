ce8b82430d87f43cdfd58fe26384bc1ec41483ed
jiwon <googims33@naver.com>
2017-01-21 15:10:03 +0900

    delete file

--- helper.h
+++
@@ -1,9 +0,0 @@
-#ifndef HELPER_H
-#define HELPER_H
-
-int input_check(int argc, char** argv);
-void init_option(int *arr, int size, char *opt);
-void sort_option(int *arr, int size, char *sort);
-
-
-#endif // HELPER_H
